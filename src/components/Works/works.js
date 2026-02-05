830af6b9ce14b77213697af34b7b7577e3c4587f
Avinash <140810654+AvinashAchar@users.noreply.github.com>
2026-02-05 17:32:55 +0530

    Update images

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './works.css';
-import Portfolio1 from '../../assets/portfolio-1.png';
-import Portfolio2 from '../../assets/portfolio-2.png';
-import Portfolio3 from '../../assets/portfolio-3.png';
-import Portfolio4 from '../../assets/portfolio-4.png';
-import Portfolio5 from '../../assets/portfolio-5.png';
-import Portfolio6 from '../../assets/portfolio-6.png';
+import Portfolio1 from '../../assets/portfolio-1new.png';
+import Portfolio2 from '../../assets/portfolio-2new.png';
+import Portfolio3 from '../../assets/portfolio-3new.png';
+import Portfolio4 from '../../assets/portfolio-4new.png';
+import Portfolio5 from '../../assets/portfolio-5new.png';
+import Portfolio6 from '../../assets/portfolio-6new.png';
 
 
 const works = () => {
