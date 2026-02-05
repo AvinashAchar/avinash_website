519927d912c938118d7cd887b6590046bd0e7f37
Avinash <140810654+AvinashAchar@users.noreply.github.com>
2026-02-05 18:04:09 +0530

    final fix: clean App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import React, { useState } from "react";
+
+
+
+
 import Navbar from "./components/NavBar/navbar";
 import Intro from './components/Intro/intro';
 import Skills from "./components/Skills/skills";
@@ -5,6 +10,7 @@ import Works from "./components/Works/works";
 import Contact from "./components/Contact/contact";
 import Footer from "./components/Footer/footer";
 function App() {
+  const [loading, setLoading] = useState(false);
   return (
     <div className="App">
         <Navbar/>
